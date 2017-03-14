19ddac5f83258ea5c39ddc9fe9f895c027259e8f
Johnson Tai <johnsont426@gmail.com>
2017-03-13 21:57:32 -0700

    complete Theater class

--- lib/movies_around_you/theater.rb
+++ lib/movies_around_you/theater.rb
@@ -9,4 +9,25 @@ class MoviesAroundYou::Theater
 		@@all << self
 	end
 
+	def movies
+		@movies.dup.freeze
+	end
+
+	def add_movie(movie)
+		if !movie.is_a?(MoviesAroundYou::Movie)
+			raise InvalidType, "Must be a Movie"
+		end
+		@movies << movie
+	end
+
+	def add_movie_from_collection(movie_array)
+		movie_array.each do |movie|
+			self.add_movie(movie)
+		end
+	end
+
+	def self.all
+		@@all
+	end
+
 end
