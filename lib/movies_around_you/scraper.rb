87ce3d6cb5f55b015caf7143039f1a38c637e247
Johnson Tai <johnsont426@gmail.com>
2017-03-13 21:15:35 -0700

    add movies_scraper method in Scraper class

--- lib/movies_around_you/scraper.rb
+++ lib/movies_around_you/scraper.rb
@@ -8,4 +8,21 @@ class MoviesAroundYou::Scraper
 		end
 	end
 
+	def movies_scraper(url)
+		html=open(url)
+		doc=Nokogiri::HTML(html)
+		doc.css('.showtimes-theater').collect do |theater|
+			theater.css(".showtimes-movie-container").collect do |movie|
+				movie_hash = {}
+				movie_hash[:title]=movie.css('.showtimes-movie-detail').css('h3').css('a')[0].text if movie.css('.showtimes-movie-detail').css('h3').css('a')[0]
+				movie_hash[:genre]=movie.css('.showtimes-movie-genre').text.strip if movie.css('.showtimes-movie-genre').text.strip!=""
+				movie_hash[:time_unavailable]=movie.css('.btn-inactive').collect{|time| time.text} if movie.css('.btn-inactive')
+				movie_hash[:time_available]=movie.css('.btn-ticket').collect{|time| time.text} if movie.css('.btn-ticket')
+				movie_hash
+			end
+			
+		end
+		
+	end
+
 end
